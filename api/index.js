0409051c36b8f9c853aaeee7198a1369c20239b2
sanikadev26 <sanikadev26@gmail.com>
2024-05-14 18:56:04 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 dotenv.config();
 
-mongoose.connect("mongodb+srv://sanikadev26:sanika@mern-estate.mbdi362.mongodb.net/?retryWrites=true&w=majority&appName=MERN-ESTATE").then(() => {
+mongoose.connect("mongodb+srv://sanikadev26:sanika26@mern-estate.mbdi362.mongodb.net/?retryWrites=true&w=majority&appName=MERN-ESTATE").then(() => {
     console.log('Connected to MongoDB!');
 }).catch((err) => {
     console.log(err);
@@ -15,3 +16,6 @@ app.listen(3000, () => {
     console.log('Server is running on port 3000!');
 }
 );
+
+
+app.use('/api/user', userRouter);
