d12da1869a4ee279f782651851a59fc4bc7053ac
sanikadev26 <sanikadev26@gmail.com>
2024-05-17 15:50:23 +0530

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -1,21 +1,34 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
-mongoose.connect("mongodb+srv://sanikadev26:sanika26@mern-estate.mbdi362.mongodb.net/?retryWrites=true&w=majority&appName=MERN-ESTATE").then(() => {
-    console.log('Connected to MongoDB!');
-}).catch((err) => {
-    console.log(err);
-})
+const connectDb = async () => {
+  const conn = await mongoose
+    .connect(
+      "mongodb+srv://sanikadev26:sanika@mern-estate.mbdi362.mongodb.net/?retryWrites=true&w=majority&appName=MERN-ESTATE"
+    )
+    .then(() => {
+      console.log("Connected to MongoDB!");
+      console.log(process.env.MONGO);
+    })
+    
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+connectDb();
 
 const app = express();
 
+app.use(express.json());
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
 app.listen(3000, () => {
-    console.log('Server is running on port 3000!');
-}
-);
-
-
-app.use('/api/user', userRouter);
+    console.log("Server is running on port 3000!");
+  });
