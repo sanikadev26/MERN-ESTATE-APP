ddeb076d191728a70a68f0f9e319ea564e526f6f
sanikadev26 <sanikadev26@gmail.com>
2024-05-18 15:23:06 +0530

    complete sign up page ui

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,6 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 
-
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
