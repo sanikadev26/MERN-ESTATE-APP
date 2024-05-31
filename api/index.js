e7d956e01646fbc4f5549bd523361c17457155b0
sanikadev26 <sanikadev26@gmail.com>
2024-05-31 22:44:29 +0530

    Create Update user Api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 const connectDb = async () => {
@@ -12,7 +13,7 @@ const connectDb = async () => {
     )
     .then(() => {
       console.log("Connected to MongoDB!");
-      console.log(process.env.MONGO);
+      // console.log(process.env.MONGO);
     })
     
     .catch((err) => {
@@ -25,6 +26,8 @@ connectDb();
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
